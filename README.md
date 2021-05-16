9c301d8e157c4c7b41b08fbe74b75507a2241471
patil-vipul <developer.vipulpatil@gmail.com>
2021-05-16 12:59:14 +0530

    Simple Mail PHP

---
+++ README.md
@@ -0,0 +1,29 @@
+# Simple PHP Script for sending mail from custom SMTP server.
+
+# Usage
+
+Set values of SMTP and Authentication in index.php
+$HOST
+$SMTP_USERNAME
+$SMTP_PASSWORD
+$SMTP_PORT
+$SEND_FROM_MAIL
+$SEND_FROM_NAME
+$SUBJECT
+$BODY
+
+Call to PHP file with following get Parameters
+email - 'Email of the recipient'
+name - 'Name of the recipient'
+
+e.g : example.com/mail.php?email=someone@example.com&&name=Someone
+
+# Note:
+
+If you rename mail.php to index.php then no need to call it with file name, for example if the file is in "mail" folder in project
+then you can call directly with folder name :
+example.com/mail?email=someone@example.com&name=Someone
+
+Further you can customize as you want ;)
+
+# Regards, Vipul Patil
---
+++ mail.php
@@ -0,0 +1,54 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+$HOST = 'mail.sit.hosting.acm.org';
+$SMTP_USERNAME = 'noreply@sit.hosting.acm.org';
+$SMTP_PASSWORD = 'SIT@ACM123';
+$SMTP_PORT = 2525;
+$SEND_FROM_MAIL = 'noreply@sit.hosting.acm.org';
+$SEND_FROM_NAME = 'ACM SIT';
+$SUBJECT = 'Thanks for showing interest in ACM SIT';
+$BODY =  "<div style='font-family:Arial, Helvetica, sans-serif;width: 100%;'>
+<p>Hello, " . $_GET['name'] . "</p>
+<p>Thank for showing interest in ACM SIT's Student Chapter</p>
+<p>Happy Exploring</p>
+<p>ACM Members</p>
+</div>";
+$BODY_ALT = "Thanks for showing interest in ACM SIT Student Chapter We'll get back to you shortly";
+
+if (isset($_GET['name']) && isset($_GET['email'])) {
+    try {
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host       = $HOST;
+        $mail->SMTPAuth   = true;
+        $mail->Username   = $SMTP_USERNAME;
+        $mail->Password   = $SMTP_PASSWORD;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = $SMTP_PORT;
+        $mail->setFrom($SEND_FROM_MAIL,);
+        $mail->addAddress($_GET['email'], $_GET['name']);
+        $mail->isHTML(true);
+        $mail->Subject = $SUBJECT;
+        $mail->Body    = $BODY;
+        $mail->AltBody = $BODY_ALT;
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+
+
+//Simple Mail Testing PHP Script,
+//developer.vipulpatil@gmail.com
